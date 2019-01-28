765bca179df376c2fc1ddda4e4895e69cd852ba0
jmartinez729 <jmartinez729@gmail.com>
2019-01-27 20:25:56 -0500

    some finishing touches

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   const birthdayTime = document.querySelector('#birthdayTime')
-  const backgroundMusicPath = '/Users/johnmartinez/dev/ninas_birthday_site/sounds/somePlaceholderMusic.mp3'
 
   const countdownContainer = document.querySelector('#countdown')
   const daysCountdown = document.querySelector('#days')
@@ -8,16 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const minutesCountdown = document.querySelector('#minutes')
   const secondsCountdown = document.querySelector('#seconds')
 
-  let backgroundMusic = []
+  const currentTime = new Date()
+  let yearOfTheEvent = currentTime.getFullYear()
+  let eventDate = new Date( yearOfTheEvent, 01, 04 )
+  const isItFebruary4th = currentTime.getMonth() === 01 && currentTime.getDate() === 04
 
   function countdown() {
-
     const now = new Date()
-    let currentYear = now.getFullYear()
-    // let eventDate = new Date( currentYear, 01, 04 )
-    let eventDate = new Date( currentYear, 00, 28 )
-    const isItFebruary4th = now.getMonth() === 0 && now.getDate() === 27
-
     const currentTime = now.getTime()
     const eventTime = eventDate.getTime()
     const remainingTime = eventTime - currentTime
@@ -31,17 +28,12 @@ document.addEventListener('DOMContentLoaded', function() {
     minutes %= 60
     seconds %= 60
 
-    const hoursView = ( hours < 10 ) ? "0" + hours : hours
-    const minutesView = ( minutes < 10 ) ? "0" + minutes : minutes
-    const secondsView = ( seconds < 10 ) ? "0" + seconds : seconds
-
     if ( isItFebruary4th ) {
       console.log('Happy birthday, Nina!')
-      countdownContainer.style.display = "none"
 
+      countdownContainer.style.display = "none"
       birthdayTime.style.display = "block"
-      backgroundMusic.play()
-      // document.body.style.backgroundColor = "pink"
+      yearOfTheEvent++
 
     } else {
 
@@ -55,26 +47,6 @@ document.addEventListener('DOMContentLoaded', function() {
     } // end of if ( isItFebruary4th )
 
   } // end of countdown
+  countdown()
 
-  function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-      this.sound.play();
-    }
-    this.stop = function(){
-      this.sound.pause();
-    }
-  } // end of sound(src)
-
-  function main() {
-    backgroundMusic = new sound(backgroundMusicPath)
-    countdown()
-  }
-
-  main()
 }) // end of DOMContentLoaded
